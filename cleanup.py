f2bfccc6b7f360c820600cc5037d6a0fc36a4c0c
Arvin Sharma <sharma.arv@husky.neu.edu>
2016-10-01 22:46:50 -0400

    cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,3 +1,5 @@
+#!/bin/env python
+
 infile = "stuff.txt"
 outfile = "out.txt"
 
@@ -5,13 +7,22 @@ fin = open(infile)
 fout = open(outfile, "w+")
 delete = False
 for line in fin:
-    if ("</a>]" in line and delete):
+    if "</a>]" in line and delete:
         delete = False
         line = ""
-    if ("[<a " in line or delete):
+    if "nike roshe" in line and not delete:
+        line = ""
+        delete = False
+    if "I kobe shoes" in line and not delete:
+        delete = False
+        line = ""
+    if "atlanta falcons" in line and not delete:
+        delete = False
+        line = ""
+    if "[<a " in line or delete:
         delete = True
         line = ""
-    if ("This post was edited by" in line):
+    if "This post was edited by" in line:
         line = ""
         
     fout.write(line)
